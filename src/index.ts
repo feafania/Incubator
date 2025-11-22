60ec0752c4ec7945bf68388dfc483a234e5ced16
feafania <173483105+feafania@users.noreply.github.com>
2025-11-22 00:10:18 +0000

    refactor: setup for vercel

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ declare global {
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (!global.__dbConnected) {
+    console.log("Mongo URL:", SETTINGS.MONGO_URL);
     await runDB(SETTINGS.MONGO_URL);
     global.__dbConnected = true;
   }
