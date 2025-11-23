4914919c7a02a5bb9fb1702e8c71db299edd26a5
feafania <173483105+feafania@users.noreply.github.com>
2025-11-23 22:35:22 +0000

    chore: add config files

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+  /** @type {import('ts-jest').JestConfigWithTsJest} **/
+module.exports = {
+  preset: 'ts-jest',
+  testTimeout: 100000, // Increase timeout for tests
+  testRegex: "__tests__/.*.e2e.test.ts$",
+  testEnvironment: "node",
+  transform: {
+    "^.+.tsx?$": ["ts-jest",{}],
+  },
+};
