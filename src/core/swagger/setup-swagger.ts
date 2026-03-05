1f938618b5da1ea7aa7dcd83bafd8404b617f719
feafania <173483105+feafania@users.noreply.github.com>
2026-03-05 22:00:50 +0000

    fix: return typescript 4

--- src/core/swagger/setup-swagger.ts
+++ src/core/swagger/setup-swagger.ts
@@ -17,5 +17,5 @@ const swaggerOptions = {
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
 
 export const setupSwagger = (app: Application) => {
-  app.use("/api", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.use("/api", swaggerUi.serve as any, swaggerUi.setup(swaggerSpec));
 };
